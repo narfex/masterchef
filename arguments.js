941c5030f9f5358019e3b75e9aacd9382e6d247b
Danil Sakhinov <ldevil2209@ya.ru>
2022-06-09 17:01:38 +0800

    Fix verify args

--- arguments.js
+++ arguments.js
@@ -1,8 +1,8 @@
 module.exports = [
 	"0xcDA8eD22bB27Fe84615f368D09B5A8Afe4a99320", // Narfex Token
-	0, // defaultRewardPerBlock
-	60 * 20, // commissionInterval
-	60 * 5, // harvestInterval
+	1000000000000000, // defaultRewardPerBlock
+	60 * 60, // commissionInterval
+	60 * 10, // harvestInterval
 	0, // earlyHarvestCommission
 	false, // isUnrewardEarlyWithdrawals
 	0, // rewardCancelInterval
